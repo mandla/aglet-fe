55220f8a69e8c7a1574801e7879f5ec661d26fd5
mandla <aldnammandla@live.com>
2023-01-18 11:45:24 +0200

    added assests with images and videos

--- aglet-fe/src/App.js
+++ aglet-fe/src/App.js
@@ -2,19 +2,13 @@ import logo from './aglet_logo.svg';
 import './App.css';
 
 function App() {
-
-  const slides = [
-    {url: 'https:localhost:3000', title, 'Beach'},
-    {url: 'https:localhost:3000', title, 'Beach'},
-    {url: 'https:localhost:3000', title, 'Beach'},
-    {url: 'https:localhost:3000', title, 'Beach'},
-    {url: 'https:localhost:3000', title, 'Beach'}
-  ]
+  
   return (
-    <div>
-      <h1>Hello</h1>
-      <ImageSlider slides={slides}/>
-    </div>
+    // <div>
+    //   <h1>Hello</h1>
+    //   <ImageSlider slides={slides}/>
+    // </div>
+    <div>Hello</div>
   );
 }
 
